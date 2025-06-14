6c7c573aec16f2eb49a04440ee0bd416d6420f5a
FavianPrueba <lucen753@gmail.com>
2025-06-14 16:36:25 -0500

    Este es mi primer codigo enviado

---
+++ prueba1.java
@@ -0,0 +1,7 @@
+public class prueba1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Yo soy jose " + i);
+        }
+    }
+}
