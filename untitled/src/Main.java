ce2c7c07aa10b3f06bcba352ca37ed24b2384f78
FavianPrueba <lucen753@gmail.com>
2025-06-14 16:56:44 -0500

    Estoy subiendo mis cambios

---
+++ untitled/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int jose =1; jose <= 10; jose++){
+            System.out.println("esto es un contador" + jose);
+        }
+
+    }
+}
