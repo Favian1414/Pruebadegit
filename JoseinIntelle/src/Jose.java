dd06ca0ecaff82d80ed9d7fc7e9fe17feda78e8c
FavianPrueba <lucen753@gmail.com>
2025-06-16 15:30:13 -0500

    estoy haciendo cambios

--- JoseinIntelle/src/Jose.java
+++ JoseinIntelle/src/Jose.java
@@ -1,6 +1,6 @@
 public class Jose {
 
     public static void main(String[] args) {
-        System.out.println("QUIERO SEGUIR APRENDIENDO EN JAVA ");
+        System.out.println("QUIERO SEGUIR APRENDIENDO EN JAVA y estoy en mi prueba final ");
     }
 }
